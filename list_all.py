018c379a306f5108f9ced7b22c34c6b083ad8a46
JJWSSS <1012109715@qq.com>
2015-06-05 23:52:16 +0800

    list_all
    list_all

--- list_all.py
+++ list_all.py
@@ -2,3 +2,25 @@ __author__ = 'JJW'
 # -*- coding: utf-8 -*-
 
 import os
+
+
+def list_all(keyword):
+    current_dir = os.listdir('.')
+    history_list = []
+    all_dir = []
+    for dirl in current_dir:
+        history_list.append('.')
+        all_dir.append(dirl)
+    history_list1 = []
+    all_dir1 = []
+    while not all_dir:
+        for i in range(0, len(all_dir)):
+            now = os.path.join(history_list[i], all_dir[i])
+            if os.path.isdir(now):
+                for dirl in os.listdir(now):
+                    history_list1.append(now)
+                    all_dir1.append(dirl)
+            else:
+                
+
+
