3b6aa6941f6626125b7482b8dfe1f4248a96ae1a
JJWSSS <1012109715@qq.com>
2015-06-07 20:02:43 +0800

    list_all
    完成了list_all

--- list_all.py
+++ list_all.py
@@ -6,6 +6,7 @@ import os
 
 def list_all(keyword):
     current_dir = os.listdir('.')
+    result = []
     history_list = []
     all_dir = []
     for dirl in current_dir:
@@ -13,7 +14,7 @@ def list_all(keyword):
         all_dir.append(dirl)
     history_list1 = []
     all_dir1 = []
-    while not all_dir:
+    while all_dir:
         for i in range(0, len(all_dir)):
             now = os.path.join(history_list[i], all_dir[i])
             if os.path.isdir(now):
@@ -21,6 +22,18 @@ def list_all(keyword):
                     history_list1.append(now)
                     all_dir1.append(dirl)
             else:
+                x = os.path.splitext(all_dir[i])
+                if keyword in x[0]:
+                    result.append(now)
+        all_dir = all_dir1
+        history_list = history_list1
+        all_dir1 = []
+        history_list1 = []
+    return result
+
+print(list_all('aaaaaaaaa'))
+
+
                 
 
 
