22c76ab771ed0f19e738296eb20e3fcbce3a7537
JJWSSS <1012109715@qq.com>
2015-06-09 22:25:28 +0800

    itertools
    完成了itertools，看到了PIL

--- itertools_test.py
+++ itertools_test.py
@@ -1,1 +1,13 @@
 __author__ = 'JJW'
+
+import itertools
+
+
+naturals = itertools.count(1)
+ns = itertools.takewhile(lambda x: x < 10, naturals)
+for i in ns:
+    print(i)
+for c in itertools.chain('ABC', 'DEF'):
+    print(c)
+for key, group in itertools.groupby('AAaBBBCCDD', lambda ch: ch.upper()):
+    print(key, list(group))
